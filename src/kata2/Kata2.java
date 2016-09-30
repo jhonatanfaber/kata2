4115117233d8cada345d49fb0a61ac7f450a7ffa
Jfaber <Jfaber@Sony-VAIO>
2016-09-30 11:53:16 +0100

    Segunda version de kata2v2 con objetos

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,19 +5,16 @@ public class Kata2 {
    
     public static void main(String[] args) {
          
-    int[] vector = {2,1,3,1,4,6,100,100,2,100};
-    HashMap<Integer, Integer> histograma = new HashMap<>();
-    for(int key:vector){
-        if(histograma.containsKey(key)){
-            histograma.put(key,histograma.get(key)+1);
-        
-        }else{
-            histograma.put(key,1);
+   // integer[] vector = {2,1,3,1,4,6,100,100,2,100};
+   // Double[] vector = {2.1,1.1,3.1,1.1,4.1,6.1,100.1,100.1,2.1,100.1};
+    String[] vector = {"Ana","Elena","Pedro","Juan","Elena","Pedro","Pepe","Pepe"};
+    
+    Histograma histo = new Histograma(vector);
+    HashMap<String, Integer> histograma = histo.getHistograma();
+    
+        // recorro las claves del histograma, no el vector del hisgrama.
+        for(String key:histograma.keySet()){
+            System.out.println(key +"-->"+ histograma.get(key));
         }
-    }
-    // recorro las claves del histograma, no el vector del hisgrama.
-    for(int key:histograma.keySet()){
-        System.out.println(key +"-->"+ histograma.get(key));
-    }
     }
 }
